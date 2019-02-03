1f0f2606f85ed502431a5aab24be9b5de40fecce
Oliver Chua <oliver.chua.aus@gmail.com>
2019-02-03 19:05:35 +1100

    Update jdk9 module code

--- SE/MyJava9ModuleConsumer/src/com/tougher/modules/user/MyClassConsumer.java
+++ SE/MyJava9ModuleConsumer/src/com/tougher/modules/user/MyClassConsumer.java
@@ -2,9 +2,17 @@ package com.tougher.modules.user;
 
 import com.tougher.modules.*;
 
+
 public class MyClassConsumer {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
 		MyClassProvider m = new MyClassProvider();
 		System.out.println(m.myMethod());
+		
+		//Can get class instance
+		Class clazz = Class.forName("com.tougher.priv.MyHiddenClass");
+		//Cannot create new instance
+		//clazz.newInstance();
+		
+		//Cannot have same package names as required module
 	}
 }
